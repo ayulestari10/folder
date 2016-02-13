9c1eecd497e4fba9718beb3555c1a687eb0515e1
ayulestari10 <lestariayu669@gmail.com>
2016-02-13 16:04:54 +0700

    initial commit

--- bel1.php
+++ bel1.php
@@ -1,10 +1,10 @@
 <?php
 
-mysql_connect('br-cdbr-azure-south-a.cloudapp.net','bd8889dfe8683e','c01f79fa');
+mysql_connect('br-cdbr-azure-south-a.cloudapp.net','bd8889dfe8683e','c01f79fa') or die(mysql_error());
 
-mysql_select_db('study');
+mysql_select_db('study') or die(mysql_error());
 
-$sql=mysql_query("SELECT * FROM user");
+$sql=mysql_query("SELECT * FROM user") or die(mysql_error());
 
 while($row= mysql_fetch_array($sql)){
 	echo $row['username']."</br>";
