e0d5b0d9754db038db9f1283607daa65f6b51194
ayulestari10 <lestariayu669@gmail.com>
2016-02-14 13:52:15 +0700

    initial commit

---
+++ register.php
@@ -0,0 +1,24 @@
+<?php
+
+mysql_connect('br-cdbr-azure-south-a.cloudapp.net','bd8889dfe8683e','c01f79fa') or die(mysql_error());
+
+mysql_select_db('belajar') or die(mysql_error());
+
+$sql=mysql_query("SELECT * FROM user") or die(mysql_error());
+
+?>
+<form>
+	<table>
+		<tr>
+			<td>Nama<td>
+			<td>Email</td>
+		</tr>
+		<tr><?php
+			while($row= mysql_fetch_array($sql)){		
+				echo "<td>".$row['Username']."<td>";
+				echo "<td>".$row['Password']."<td>";
+			}
+			?>
+		</tr>
+			</table>
+</form>
